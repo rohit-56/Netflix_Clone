01be55e43f8248ac40947ae3057d3d100060ea4c
rohit-56 <rohitmou25@gmail.com>
2022-12-05 22:14:21 +0530

    main tab bar implementation

---
+++ Netflix/Controller/MainTabBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarViewController.swift
+//  Netflix
+//
+//  Created by Rohit Sharma on 05/12/22.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.to.down.line")
+       
+        vc1.title = "Home"
+        vc2.title = "UpComing"
+        vc3.title = "Search"
+        vc4.title = "Downloads"
+        
+        setViewControllers([vc1,vc2,vc3,vc4], animated: true)
+        
+        
+        
+    }
+
+}
