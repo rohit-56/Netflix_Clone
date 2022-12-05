2448e501aba1f0addabf7f6260c041009050001c
rohit-56 <rohitmou25@gmail.com>
2022-12-05 22:14:06 +0530

    add all the tab bar items

---
+++ Netflix/Controller/DownloadsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadsViewController.swift
+//  Netflix
+//
+//  Created by Rohit Sharma on 05/12/22.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+
+}
---
+++ Netflix/Controller/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Netflix
+//
+//  Created by Rohit Sharma on 05/12/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
