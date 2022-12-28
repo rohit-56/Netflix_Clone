c96925ed588f48d3d72cb5ff74ad80ee98821625
rohit-56 <rohitmou25@gmail.com>
2022-12-28 12:10:06 +0530

    add tableView in search VC

--- Netflix/Controller/SearchViewController.swift
+++ Netflix/Controller/SearchViewController.swift
@@ -8,11 +8,53 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    
+    private var serachTableView : UITableView = {
+       let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        
+        view.backgroundColor = .systemBackground
+        
+        serachTableView.delegate = self
+        serachTableView.dataSource = self
+        
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        
+        view.addSubview(serachTableView)
+        
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        serachTableView.frame = view.bounds
+    }
+    
+}
 
+extension SearchViewController : UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .magenta
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
 }
