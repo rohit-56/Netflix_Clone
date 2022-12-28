b3c5e56df94cd1db2418b73c2074170b98e7f2e0
rohit-56 <rohitmou25@gmail.com>
2022-12-28 12:10:42 +0530

    create search Result VC to show search results of movies/tvs

---
+++ Netflix/Controller/General/SearchResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix
+//
+//  Created by Rohit Sharma on 28/12/22.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .darkGray
+    }
+    
+}
