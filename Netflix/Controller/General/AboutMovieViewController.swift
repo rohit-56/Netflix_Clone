43dbe73bf81e9cddbbbad0b5bf0699d74e908a28
rohit-56 <rohitmou25@gmail.com>
2022-12-29 16:04:05 +0530

    create new VC to show when tap on movies/tv shows

---
+++ Netflix/Controller/General/AboutMovieViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutMovieViewController.swift
+//  Netflix
+//
+//  Created by Rohit Sharma on 29/12/22.
+//
+
+import UIKit
+
+class AboutMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .purple
+    }
+
+}
