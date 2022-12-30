9536919a736ab828a878a6f689ec02bafc174bf5
rohit-56 <rohitmou25@gmail.com>
2022-12-30 17:14:58 +0530

    create new class to store all API-key

---
+++ Netflix/API_Key.swift
@@ -0,0 +1,12 @@
+
+
+import Foundation
+
+
+class Constants {
+    static let API_Key = "b430b4d0163c23106730cf79358c8f41"
+    static let Base_Url = "https://api.themoviedb.org/3/"
+    static let YouTube_API_Key = "AIzaSyACKRpiOS9IkGeOAED7dBBQns9zwsaKyuU"
+    static let YouTube_Base_Url = "https://youtube.googleapis.com/youtube/v3"
+}
+
