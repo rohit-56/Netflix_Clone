01374ebde502fb931e89e495952fa1dc39d3db18
rohit-56 <rohitmou25@gmail.com>
2023-01-01 14:52:52 +0530

    create table view to show downloaded data

--- Netflix/Controller/DownloadsViewController.swift
+++ Netflix/Controller/DownloadsViewController.swift
@@ -8,11 +8,61 @@
 import UIKit
 
 class DownloadsViewController: UIViewController {
+    
+    private var titles = [TitleModel]()
+    
+    private var downloadedMovies : UITableView = {
+        let tableView = UITableView()
+        tableView.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        
+        title = "Download"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(downloadedMovies)
+        
+        downloadedMovies.delegate = self
+        downloadedMovies.dataSource = self
+        showDownloadedMovies()
     }
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        downloadedMovies.frame = view.bounds
+    }
+    func showDownloadedMovies(){
+        DataPersistenceManager.shared.fetchTitleModelList{ results in
+            switch results{
+            case .success(let titleModel):
+                self.titles = titleModel
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            
+        }
+    }
+}
+extension DownloadsViewController : UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard  let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {return UITableViewCell()}
+        
+        cell.configure(with: TitleViewModel(movie: titles[indexPath.row].original_title ?? titles[indexPath.row].title ?? "Unknown", poster: titles[indexPath.row].poster_path ?? "Unknown"))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        150
+    }
+    
+    
 }
